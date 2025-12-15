21a1a732ea1e3501cb58b70e003cf72a5c6b01f7
송민석 <minsk0429@songminseog-ui-MacBookAir.local>
2025-12-15 21:33:09 +0900

    코인 시스템, 운세 시스템, TV 아이콘 추가

---
+++ src/CustomStatus.java
@@ -0,0 +1,10 @@
+public class CustomStatus {
+
+	// 이제 사용자님이 원하셨던 State의 멤버 변수들을 넣습니다.
+	public int width;       
+	public int height;      
+	public int start_x;     
+	public int start_y;     
+	public int frame_size;  
+	// 이 클래스 내부에 public static class State를 다시 넣으면 안 됩니다!
+}
